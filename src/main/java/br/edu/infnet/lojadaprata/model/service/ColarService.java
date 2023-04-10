3ff3945a55f164890608cefe71fdde33221e0242
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:22:55 -0300

    Update ColarService.java

--- src/main/java/br/edu/infnet/lojadaprata/model/service/ColarService.java
+++ src/main/java/br/edu/infnet/lojadaprata/model/service/ColarService.java
@@ -1,9 +1,12 @@
 package br.edu.infnet.lojadaprata.model.service;
 
+
 import br.edu.infnet.lojadaprata.model.domain.Colar;
+import br.edu.infnet.lojadaprata.model.domain.Pulseira;
 import br.edu.infnet.lojadaprata.model.domain.Usuario;
 import br.edu.infnet.lojadaprata.model.repository.ColarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -19,17 +22,26 @@ public class ColarService {
 	}
 
 	public void apagar(Integer id) {
-		colarRepository.deleteById(id);
+		if(colarRepository.existsById(id)) {
+			colarRepository.deleteById(id);
+		} else {
+			throw new IllegalArgumentException("Registro não encontrado");
+		}
 	}
 
 	public Collection<Colar> listagem(){
-		return (Collection<Colar>) colarRepository.findAll();
+		return (Collection<Colar>) colarRepository.findAll(Sort.by(Sort.Direction.ASC, "nomeProduto"));
 	}
-
 	public Collection<Colar> listagem(Usuario cadastro){
 		return (Collection<Colar>) colarRepository.listagem(cadastro.getId());
 	}
-	public Colar obterPorId(Integer id) {
+	public Colar buscarPorId(Integer id) {
 		return colarRepository.findById(id).orElse(null);
+	}
+	public void atualizar(Colar colar) {
+		colarRepository.save(colar);
+	}
+	public Collection<Colar> listagem2(){
+		return colarRepository.findAll(Sort.by(Sort.Direction.ASC, "modelo"));
 	}
 }
