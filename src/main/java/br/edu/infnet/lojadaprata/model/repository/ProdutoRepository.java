8e9521896ddbed141c3dd39fc3cab567fb07255e
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:21:24 -0300

    Update ProdutoRepository.java

--- src/main/java/br/edu/infnet/lojadaprata/model/repository/ProdutoRepository.java
+++ src/main/java/br/edu/infnet/lojadaprata/model/repository/ProdutoRepository.java
@@ -1,6 +1,8 @@
 package br.edu.infnet.lojadaprata.model.repository;
 
+import br.edu.infnet.lojadaprata.model.domain.Anel;
 import br.edu.infnet.lojadaprata.model.domain.Produto;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,5 @@ public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
 
 	@Query("from Produto p where p.cadastro.id = :userId")
 	List<Produto> listagem(Integer userId);
+	List<Produto> findAll(Sort sort);
 }
