f36799ea893d5277a17ba7629469a0ba1208eb25
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:19:54 -0300

    Update ClienteRepository.java

--- src/main/java/br/edu/infnet/lojadaprata/model/repository/ClienteRepository.java
+++ src/main/java/br/edu/infnet/lojadaprata/model/repository/ClienteRepository.java
@@ -1,6 +1,8 @@
 package br.edu.infnet.lojadaprata.model.repository;
 
+import br.edu.infnet.lojadaprata.model.domain.Anel;
 import br.edu.infnet.lojadaprata.model.domain.Cliente;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,5 @@ public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
 
 	@Query("from Cliente c where c.cadastro.id = :userId")
 	List<Cliente> listagem(Integer userId);
+	List<Cliente> findAll(Sort sort);
 }
