fb7fe1a95597277cf82e0e9146d9e15d6b198b14
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:21:00 -0300

    Update PedidoRepository.java

--- src/main/java/br/edu/infnet/lojadaprata/model/repository/PedidoRepository.java
+++ src/main/java/br/edu/infnet/lojadaprata/model/repository/PedidoRepository.java
@@ -1,6 +1,8 @@
 package br.edu.infnet.lojadaprata.model.repository;
 
+import br.edu.infnet.lojadaprata.model.domain.Anel;
 import br.edu.infnet.lojadaprata.model.domain.Pedido;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,5 @@ public interface PedidoRepository extends CrudRepository<Pedido, Integer> {
 
 	@Query("from Pedido p where p.cadastro.id = :userId")
 	List<Pedido> obterLista(Integer userId);
+
 }
