6a6bfe8302ec00230845dccd4d16c0fbaf07f301
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:13:19 -0300

    Update PedidoController.java

--- src/main/java/br/edu/infnet/lojadaprata/controller/PedidoController.java
+++ src/main/java/br/edu/infnet/lojadaprata/controller/PedidoController.java
@@ -2,6 +2,7 @@ package br.edu.infnet.lojadaprata.controller;
 
 import br.edu.infnet.lojadaprata.model.domain.Pedido;
 import br.edu.infnet.lojadaprata.model.domain.Usuario;
+import br.edu.infnet.lojadaprata.model.service.ClienteService;
 import br.edu.infnet.lojadaprata.model.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,25 +18,29 @@ public class PedidoController {
 	@Autowired
 	private PedidoService pedidoService;
 
+	@Autowired
+	private ClienteService clienteService;
+
 	@GetMapping(value = "/pedido")
 	public String telaCadastro() {
-		return "pedido/cadastro";
+		return "pedido/cadastroPedido";
 	}
-	@GetMapping(value = "/pedido/lista")
+	@GetMapping(value = "/listaPedido")
 	public String telaLista(Model model, @SessionAttribute("cadastro") Usuario cadastro) {
 		model.addAttribute("pedidos", pedidoService.listagem(cadastro));
-		return "pedido/lista";
+		model.addAttribute("clientes", clienteService.listagem());
+		return "pedido/listaPedido";
 	}
-	@PostMapping(value = "/pedido/incluir")
+	@PostMapping(value = "/pedido/novo")
 	public String novoPedido(Pedido pedido, @SessionAttribute("cadastro") Usuario cadastro) {
 		pedido.setCadastro(cadastro);
 		pedidoService.novo(pedido);
-		return "redirect:/pedido/lista";
+		return "redirect:/listaPedido";
 	}
-	@GetMapping(value = "/pedido/{id}/excluir")
+	@GetMapping(value = "/pedido/{id}/apagar")
 	public String apagarPedido(@PathVariable Integer id) {
 		Pedido pedido = pedidoService.obterPorId(id);
 		pedidoService.apagar(id);
-		return "redirect:/pedido/lista";
+		return "redirect:/listaPedido";
 	}
 }
