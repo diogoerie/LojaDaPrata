64dc769df189d06d6fcd8b14be7cd96e7ee80297
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:20:09 -0300

    Update ColarRepository.java

--- src/main/java/br/edu/infnet/lojadaprata/model/repository/ColarRepository.java
+++ src/main/java/br/edu/infnet/lojadaprata/model/repository/ColarRepository.java
@@ -1,6 +1,8 @@
 package br.edu.infnet.lojadaprata.model.repository;
 
+import br.edu.infnet.lojadaprata.model.domain.Anel;
 import br.edu.infnet.lojadaprata.model.domain.Colar;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,5 @@ import java.util.List;
 public interface ColarRepository extends CrudRepository<Colar, Integer> {
 	@Query("from Colar c where c.cadastro.id = :userId")
 	List<Colar> listagem(Integer userId);
+	List<Colar> findAll(Sort sort);
 }
