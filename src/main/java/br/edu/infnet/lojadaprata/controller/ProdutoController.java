eb3ef8d40a9dd026cf71c511282676ace500027c
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:13:38 -0300

    Update ProdutoController.java

--- src/main/java/br/edu/infnet/lojadaprata/controller/ProdutoController.java
+++ src/main/java/br/edu/infnet/lojadaprata/controller/ProdutoController.java
@@ -1,5 +1,6 @@
 package br.edu.infnet.lojadaprata.controller;
 
+import br.edu.infnet.lojadaprata.model.domain.Anel;
 import br.edu.infnet.lojadaprata.model.domain.Produto;
 import br.edu.infnet.lojadaprata.model.domain.Usuario;
 import br.edu.infnet.lojadaprata.model.service.ProdutoService;
@@ -10,24 +11,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class ProdutoController {
 
 	@Autowired
 	private ProdutoService produtoService;
+
 	@GetMapping(value = "/listaProduto")
-	public String telaLista(Model model, @SessionAttribute("cadastro") Usuario cadastro) {
+	public String telaLista(Model model, HttpSession session) {
+		Usuario cadastro = (Usuario) session.getAttribute("cadastro");
 		model.addAttribute("produtos", produtoService.listagem(cadastro));
 		return "produtos/listaProduto";
 	}
+
 	@GetMapping(value = "/produtoCadastro")
 	public String telaCadastroAnel() {
 		return "redirect:/";
 	}
-	@GetMapping(value = "/produto/{id}/excluir")
+
+	@GetMapping(value = "/produto/{id}/apagar")
 	public String apagarProduto(@PathVariable Integer id) {
 		Produto produto = produtoService.obterPorId(id);
 		produtoService.apagar(id);
-		return "redirect:/produto/lista";
+		return "redirect:/listaProduto";
 	}
+
+
 }
