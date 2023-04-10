6f3cca818d78f88f863d9ccd759f9fdd12de6bef
diogoerie <119828335+diogoerie@users.noreply.github.com>
2023-04-09 21:11:20 -0300

    Update AnelController.java

--- src/main/java/br/edu/infnet/lojadaprata/controller/AnelController.java
+++ src/main/java/br/edu/infnet/lojadaprata/controller/AnelController.java
@@ -4,12 +4,11 @@ import br.edu.infnet.lojadaprata.model.domain.Anel;
 import br.edu.infnet.lojadaprata.model.domain.Usuario;
 import br.edu.infnet.lojadaprata.model.service.AnelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -23,16 +22,23 @@ public class AnelController {
 		return "anel/cadastroAnel";
 	}
 	@PostMapping("/anel/novo")
-	public String novoAnel(Anel anel,@SessionAttribute("cadastro") Usuario cadastro) {
+	public String novoAnel(Anel anel, @SessionAttribute("cadastro") Usuario cadastro, RedirectAttributes redirectAttributes) {
 		anel.setCadastro(cadastro);
 		anelService.novo(anel);
+		redirectAttributes.addFlashAttribute("mensagem", "Anel cadastrado com sucesso!");
 		return "redirect:/listaAnel";
 	}
+
 	@GetMapping(value = "/anel/{id}/apagar")
-	public String apagarAnel(@PathVariable Integer id) {
-		Anel anel = anelService.obterPorId(id);
-		anelService.apagar(id);
-		return "redirect:/listaAnel";
+	public String apagarAnel(@PathVariable Integer id, Model model) {
+		try {
+			Anel anel = anelService.buscarPorId(id);
+			anelService.apagar(id);
+			return "redirect:/listaAnel";
+		} catch (Exception e) {
+			model.addAttribute("mensagemErro", "Não foi possível excluir o anel devido a um erro: " + e.getMessage());
+			return "anel/listaAnel";
+		}
 	}
 	@GetMapping("/listaAnel")
 	public String telaListaDeAneis(Model model) {
